9f12b6034aa5b4da7405f030101b14f9b2d41286
Muhammad Irwanto <muhammad.irwanto95@gmail.com>
2023-05-05 19:12:06 +0700

    feat: add calculator view

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,19 @@
+﻿using Calculator.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculator(CalculatorModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ Models/CalculatorModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Calculator.Models
+{
+    public class CalculatorModel
+    {
+        public string Expression { get; set; }
+        public string Result { get; set; }
+    }
+}
