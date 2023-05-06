f4aa8d262d9757de9a0b1ea0eb63f04f5408ea2f
Muhammad Irwanto <muhammad.irwanto95@gmail.com>
2023-05-06 11:46:10 +0700

    feat: add integration test to assign expression, do calculation, and clear expression

--- Calculator.Test/CommonCalculatorTest.cs
+++ Calculator.Test/CommonCalculatorTest.cs
@@ -35,7 +35,7 @@ namespace Calculator.Test
                     Assert.Fail("Invalid model type");
                 }
 
-                Assert.Equal(model.Expression, expected);
+                Assert.Equal(expected, model.Expression);
 
                 controller.ClearExpression();
             }
@@ -59,7 +59,7 @@ namespace Calculator.Test
                     Assert.Fail("Invalid model type");
                 }
 
-                Assert.Equal(model.Expression, string.Empty);
+                Assert.Equal(string.Empty, model.Expression);
             }
         }
 
@@ -84,7 +84,108 @@ namespace Calculator.Test
                     Assert.Fail("Invalid model type");
                 }
 
-                Assert.Equal(model.Expression, expected);
+                Assert.Equal(expected, model.Expression);
+
+                controller.ClearExpression();
+            }
+        }
+
+        [Fact]
+        public void IntegrationTest_AddExpressions_And_Calculate_Then_Clear_Return_Success()
+        {
+            using (var calculator = _serviceProvider.GetService<ICommonCalculator>())
+            {
+                Assert.True(calculator != null, "CommonCalculator doesn't exist");
+
+                var controller = new CommonCalculatorController(calculator);
+
+                // Assign 2 expected 2
+                var result = controller.AssignExpression("2");
+
+                var model = ((ViewResult)result).Model as CommonCalculatorModel;
+                if (model == null)
+                {
+                    Assert.Fail("Invalid model type");
+                }
+
+                Assert.Equal("2", model.Expression);
+
+                // Assign + expected 2+
+                result = controller.AssignExpression("+");
+
+                model = ((ViewResult)result).Model as CommonCalculatorModel;
+                if (model == null)
+                {
+                    Assert.Fail("Invalid model type");
+                }
+
+                Assert.Equal("2+", model.Expression);
+
+                // Assign 4 expected 2+4
+                result = controller.AssignExpression("4");
+
+                model = ((ViewResult)result).Model as CommonCalculatorModel;
+                if (model == null)
+                {
+                    Assert.Fail("Invalid model type");
+                }
+
+                Assert.Equal("2+4", model.Expression);
+
+                // Assign . expected 2+4.
+                result = controller.AssignExpression(".");
+
+                model = ((ViewResult)result).Model as CommonCalculatorModel;
+                if (model == null)
+                {
+                    Assert.Fail("Invalid model type");
+                }
+
+                Assert.Equal("2+4.", model.Expression);
+
+                // Assign 6 expected 2+4.6
+                result = controller.AssignExpression("6");
+
+                model = ((ViewResult)result).Model as CommonCalculatorModel;
+                if (model == null)
+                {
+                    Assert.Fail("Invalid model type");
+                }
+
+                Assert.Equal("2+4.6", model.Expression);
+
+                // Assign . (again) expected 2+4.6
+                result = controller.AssignExpression(".");
+
+                model = ((ViewResult)result).Model as CommonCalculatorModel;
+                if (model == null)
+                {
+                    Assert.Fail("Invalid model type");
+                }
+
+                Assert.Equal("2+4.6", model.Expression);
+
+                // Do calculation expected 6.6
+                result = controller.DoCalculation();
+
+                model = ((ViewResult)result).Model as CommonCalculatorModel;
+                if (model == null)
+                {
+                    Assert.Fail("Invalid model type");
+                }
+
+                Assert.Equal("6.6", model.Expression);
+
+                // Clear expression expected ''
+                result = controller.ClearExpression();
+
+                model = ((ViewResult)result).Model as CommonCalculatorModel;
+                if (model == null)
+                {
+                    Assert.Fail("Invalid model type");
+                }
+
+                Assert.Equal(string.Empty, model.Expression);
             }
         }
     }
